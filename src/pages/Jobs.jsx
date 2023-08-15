8f98a54e72940f16043ad2b5118ee0a0fc09e138
bm9avan <mggpavan@gmail.com>
2023-08-15 23:29:19 +0530

    trying spring boot

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Jobs = () => {
-
+  fetch("http://localhost:8080/get").then((v)=>v.json()).then((v)=>console.log(v))
   return (
     <div style={{fontSize:"100px"}}>
       jobs
